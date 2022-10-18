76ecad81506ab9e88d5d28a2f0eb9f00ff533df0
PhucTG <tgphuc257@gmail.com>
2022-10-18 10:53:51 +0700

    add migration

--- database/migrations/2022_10_09_044121_create-roles-table.php
+++ database/migrations/2022_10_09_044121_create-roles-table.php
@@ -13,7 +13,12 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        //
+	    Schema::create('roles', function (Blueprint $table) {
+		    $table->primaryColumn();
+		    $table->string('name');
+		    $table->string('description');
+		    $table->defaultFields();
+	    });
     }
 
     /**
@@ -23,6 +28,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        //
+	    Schema::dropIfExists('roles');
     }
 }
