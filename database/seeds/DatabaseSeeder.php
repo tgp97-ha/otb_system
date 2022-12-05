5aee04ac90a5a4c3812896c56834afd99995b249
Sang Do <dovansang97@gmail.com>
2022-12-05 09:32:59 +0700

    Add TourSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Seeders\TourSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +16,6 @@ class DatabaseSeeder extends Seeder
 		$this->call(UserSeeder::class);
 		$this->call(PlacesSeeder::class);
 		$this->call(ServiceSeeder::class);
+		$this->call(TourSeeder::class);
 	}
 }
