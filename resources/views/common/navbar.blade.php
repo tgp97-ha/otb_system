526647e808d9100265bd1d7f37e1b1ceb91d95e3
Sang Do <dovansang97@gmail.com>
2022-12-17 16:56:28 +0700

    update navbar

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -1,46 +1,49 @@
-<nav class="fixed top-0 left-0 z-50 w-full h-20 px-2 pt-1 bg-gray-900 border-gray-700">
-    <div class="flex flex-wrap items-center justify-between">
-        <a href="#" class="w-32 flex items-center">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Pornhub-logo.svg" alt="OTB Logo" />
-        </a>
-
-        <div class="" id="navbar-dropdown">
-            <ul class="flex flex-col p-4 border rounded-lg bg-gray-800 border-gray-700">
-                <li class="relative">
-                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-                        class="flex items-center justify-between w-fullfont-medium  rounded text-gray-400 hover:text-white focus:text-white border-gray-700 hover:bg-gray-700">{{ 'Hello ' . auth()->user()->username }}
-                        <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd"></path>
-                        </svg></button>
-                    <!-- Dropdown menu -->
-                    <div id="dropdownNavbar"
-                        class="absolute top-10 -right-6 z-10 hidden font-normal divide-y rounded shadow w-44 bg-gray-700 divide-gray-600">
-                        <ul class="py-1 text-sm text-gray-400">
-                            <li>
-                                <a href="{{ url('tour-operator/profile', []) }}"
-                                    class="block px-4 py-2 hover:bg-gray-600 hover:text-white">Profile</a>
-                            </li>
-                            <li>
-                                <a href="#"
-                                    class="block px-4 py-2 hover:bg-gray-600 hover:text-white">Settings</a>
-                            </li>
-                        </ul>
-                        <div class="py-1">
-                            <a href="{{ url('/logout') }}"
-                                onclick="event.preventDefault();document.getElementById('logout-form').submit();"
-                                class="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-400 hover:text-white">Sign
-                                out</a>
-                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                        </div>
-
-                    </div>
-                </li>
-            </ul>
+<nav
+    class="fixed top-0 left-0 w-full flex flex-wrap items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light z-50">
+    <div class="container-fluid w-full flex flex-wrap items-center justify-between px-2">
+        <button
+            class="navbar-toggler text-gray-200 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
+            type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent1"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="w-6" role="img"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path fill="currentColor"
+                    d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+                </path>
+            </svg>
+        </button>
+        <div class="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent1">
+            <a class="text-xl text-white pr-2 font-semibold" href="#">Online Tour Booking</a>
         </div>
+        <!-- Collapsible wrapper -->
+
+        <!-- Right elements -->
+        <div class="flex items-center relative">
+            <!-- Icon -->
+            <span>{{ 'Hello ' . auth()->user()->username }}</span>
+            <div class="dropdown relative ml-3">
+                <a class="dropdown-toggle flex items-center hidden-arrow" href="#" id="dropdownMenuButton2"
+                    role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://mdbootstrap.com/img/new/avatars/2.jpg" class="rounded-full"
+                        style="height: 25px; width: 25px" alt="" loading="lazy" />
+                </a>
+                <ul class="dropdown-menu min-w-max absolute bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none left-auto right-0"
+                    aria-labelledby="dropdownMenuButton2">
+                    <li>
+                        <a class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                            href="#">Action</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                            href="#">Another action</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                            href="#">Something else here</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- Right elements -->
     </div>
 </nav>
